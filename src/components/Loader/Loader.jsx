81f8956113032b8902f85e462ce3ef747ac58a00
Piojank <98431706+Piojank@users.noreply.github.com>
2022-11-09 22:43:37 +0100

    fix bug

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import { Audio } from  'react-loader-spinner'
-import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 const Loader = () => {
     return (
