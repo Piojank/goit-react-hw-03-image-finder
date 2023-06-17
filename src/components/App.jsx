651e4df47e9347b3443aff58a0e3c6fc64fb9750
piojank <p.jankowski931026@gmail.com>
2023-06-18 00:05:54 +0200

    Add searchbar to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+
+import Searchbar from './Searchbar/Searchbar';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default class App extends Component {
+
+    state = {
+        searchRequest: '',
+    }
+
+    render() {
+        return (
+            <>
+                <Searchbar onSearch={this.handleSearchSubmit} />
+
+                <ToastContainer autoClose={3000} />
+            </>
+        );
+    };
 };
