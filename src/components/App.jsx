e92b174ffc11071c9b53f8235c83a528ee9e6e4d
piojank <p.jankowski931026@gmail.com>
2023-06-20 00:32:18 +0200

    Create App class

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
-import Searchbar from './Searchbar/Searchbar';
-import { Button } from './Button/Button';
-import { Loader } from './Loader/Loader';
-
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Searchbar from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { Button } from './Button/Button';
+import Modal from './Modal/Modal';
+import { Loader } from './Loader/Loader';
+import { fetchImages } from 'utils/PixabayAPI';
 export default class App extends Component {
 
     state = {
@@ -18,16 +20,101 @@ export default class App extends Component {
         showModal: null,
     };
 
+    componentDidUpdate(_, prevState) {
+        const prevSearch = prevState.searchRequest;
+        const currentSearch = this.state.searchRequest;
+        const prevGalleryPage = prevState.galleryPage;
+        const currentGalleryPage = this.state.galleryPage;
+
+        if (
+            prevSearch !== currentSearch || 
+            prevGalleryPage !== currentGalleryPage
+        ) {
+            this.updateImages();
+        };
+    };
+
+    updateImages() {
+        const { searchRequest, galleryPage } = this.state;
+        this.setState({ isLoading: true });
+        setTimeout(() => {
+        try {
+            fetchImages(searchRequest, galleryPage).then(data => {
+            if (!data.data.hits.length) {
+                return toast.error(
+                'There is no images found with that search request'
+                );
+            }
+            const mappedImages = data.data.hits.map(
+                ({ id, webformatURL, tags, largeImageURL }) => ({
+                id,
+                webformatURL,
+                tags,
+                largeImageURL,
+                })
+            );
+            this.setState({
+                images: [...this.state.images, ...mappedImages],
+            });
+            });
+        } catch (error) {
+            this.setState({ error });
+        } finally {
+            this.setState({ isLoading: false });
+        }
+        }, 1000);
+    }
+
+    handleSearchSubmit = searchRequest => {
+        this.setState({
+            searchRequest,
+            images: [],
+            galleryPage: 1,
+        });
+    };
+
+    loadMore = () => {
+        this.setState(prevState => ({
+            galleryPage: prevState.galleryPage + 1,
+        }));
+    };
+
+    showModalImage = id => {
+        const image = this.state.images.find(image => image.id === id);
+        this.setState({
+            showModal: {
+                largeImageURL: image.largeImageURL,
+                tags: image.tags,
+            },
+        });
+    };
+
+    closeModalImage = () => {
+        this.setState({ showModal: null });
+    };
+
     render() {
+        const { images, error, isLoading, showModal} = this.state;
+
         return (
             <>
                 <Searchbar onSearch={this.handleSearchSubmit} />
-
-                <>
-                    <Button loadMore={this.loadMore}/>
-                </>
-
-                <ToastContainer autoClose={3000} />
+                {error && toast.error(`Whoops, something went wrong: ${error.message}`)}
+                {isLoading && <Loader color={'#3f51b5'} size={32} />}
+                {images.length > 0 && (
+                    <>
+                        <ImageGallery images={images} handlePreview={this.showModalImage} />
+                        <Button loadMore={this.loadMore}/>
+                    </>
+                )}
+                {showModal && (
+                    <Modal
+                        urlImage={showModal.largeImageURL}
+                        tags={showModal.tags}
+                        closeModal={this.closeModalImage}
+                    />
+                )}
+                <ToastContainer autoClose={4000} />
             </>
         );
     };
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,5 +3,44 @@ import PropTypes from 'prop-types';
 import s from './Modal.module.css';
 
 export default class Modal extends Component {
+    static propTypes = {
+        closeModal: PropTypes.func.isRequired,
+        urlImage: PropTypes.string.isRequired,
+        tags: PropTypes.string.isRequired,
+    };
 
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleKeyDown);
+    };
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleKeyDown);
+    };
+
+    handleKeyDown = element => {
+        if (element.code === 'Escape') {
+            this.props.closeModal();
+        };
+    };
+
+    handleBackdropClick = event => {
+        if (event.currentTarget === event.target) {
+            this.props.closeModal();
+        };
+    };
+
+    render() {
+        const { urlImage, tags } = this.props;
+
+        return (
+            <div 
+                className={s.Overlay}
+                onClick={this.handleBackdropClick}
+            >
+                <div className={s.Modal}>
+                    <img src={urlImage} alt={tags}/>
+                </div>
+            </div>
+        );
+    };
 };
