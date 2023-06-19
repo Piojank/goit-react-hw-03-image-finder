b85c09a347c444528ff25514b0194a611be31f8d
piojank <p.jankowski931026@gmail.com>
2023-06-20 01:32:28 +0200

    fix api

--- src/utils/PixabayAPI.js
+++ src/utils/PixabayAPI.js
@@ -1,16 +1,13 @@
 import axios from 'axios';
 
-const settings = {
-    BASE_URL: 'https://pixabay.com/api/?',
-    API_KEY: 'key=29712287-5d117bcccbf45424d50c3eb4b',
-    TYPE: '&image_type=photo',
-    PER_PAGE: '&per_page=12',
-};
-
-const { BASE_URL, API_KEY, TYPE, PER_PAGE } = settings;
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '29712287-5d117bcccbf45424d50c3eb4b';
+const IMAGE_TYPE = 'image_type=photo';
+const IMAGE_SETTINGS = 'orientation=horizontal&safesearch=true';
+const PER_PAGE = 'per_page=12';
 
 export const fetchImages = async (name, galleryPage) => {
-    const FETCH_URL = `${BASE_URL}${API_KEY}&q=${name}${TYPE}&orientation=horizontal&safesearch=true&page=${galleryPage}&${PER_PAGE}`;
+    const FETCH_URL = `${BASE_URL}?key=${API_KEY}&q=${name}&${IMAGE_TYPE}&${IMAGE_SETTINGS}&page=${galleryPage}&${PER_PAGE}`;
 
     return axios.get(FETCH_URL)
     .then(response => response.data)
